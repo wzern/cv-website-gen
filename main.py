4641abfafe687030bedfd0088f7799711d7d82ac
William Zernikow <me+github@wzernikow.com>
2026-02-17 17:58:22 +1300

    Add core logic to check parser works as intended

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from cv-website-gen!")
+from ollama import chat
+from ollama import ChatResponse
+from modules.parser import extract_text_from_cv
+from modules.extractor import extract_cv_data
 
-
-if __name__ == "__main__":
-    main()
+cvs = extract_text_from_cv()
+for filename, cv_text in cvs.items():
+    print(f"--- {filename} ---")
+    cv_data = extract_cv_data(cv_text)
+    print(cv_data)
